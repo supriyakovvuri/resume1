a304ec7f6dde4bd61840efae6d2bfde8f83a89a8
supriyakovvuri <supriyasupriya480@gmail.com>
2018-03-22 11:20:25 +0530

    resume

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ let data=JSON.parse(text);
 console.log(data);
 ba(data.basics);
 educa(data.education);
+skill(data.skills);
 })
 //main div class calling from html
 var main=document.querySelector('.main');
@@ -60,7 +61,6 @@ for(i in education){
   var e1=document.createElement("div");
   e1.classList.add("edu1");
   e1.textContent=education[i].course;
-
   //list creation
   var ul=document.createElement("ul");
   ul.classList.add("edu2");
@@ -72,4 +72,20 @@ for(i in education){
   ul.appendChild(li);
 }
 }
+}
+function skill(skills){
+var skill_title=document.createElement("h1");
+skill_title.textContent="skill set:";
+ // skills_title.appendChild(document.createElement("HR"));
+right.appendChild(skill_title);
+
+var table=document.createElement("table");
+table.classList.add("table");
+skill_title.appendChild(table);
+var row="";
+for (var i = 0; i < skills.length; i++) {
+  row +="<tr><td>"+skills[i].name+"</td><td>"+skills[i].value+"</td></tr>";
+}
+table.innerHTML=row;
+
 }
